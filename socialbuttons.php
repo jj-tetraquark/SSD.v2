99ad8d6de5ca900d00e056554f73f4525055a05f
Jonny <jonny@Jonny-PC-II.(none)>
2012-11-14 20:21:10 +0000

    Moved the social media buttons to their own file

---
+++ socialbuttons.php
@@ -0,0 +1,34 @@
+<div id="socialbuttons">
+  <ul class="socialbuttons"> 
+    <li class="facebook"><a href="http://www.facebook.com/pages/Superstar-Destroyer-Records/178051528904115" title="Facebook"><span class="displace">Facebook</span></a>
+      <div style="display:none;" class="fb-like-box" data-href="http://www.facebook.com/ssdrecords" data-width="292" data-height="420" data-show-faces="true" data-stream="true" data-header="true"></div>
+    </li>
+    <li class="twitter"><a href="http://twitter.com/ssdrecords" title="Twitter"><span class="displace">Twitter</span></a>
+     <div class="tweet" style="display:none;">
+     <img src="http://si0.twimg.com/profile_images/1219975167/SSD_Silhouette_A5__normal.jpg" style="margin-left:30px;"/>
+     <h4><a href="http://www.twitter.com/ssdrecords" style="margin-left:40px;">@SSDRECORDS</a></h4>
+       <p>
+        <ul id="twitter_update_list">
+              <li>Twitter feed loading</li>
+        </ul> 
+        </p>
+      </div>
+   </li>
+    <li class="mixcloud"><a href="http://www.mixcloud.com/superstar_destroyer/" title="Mixcloud Podcast"><span class="displace">Mixcloud</span></a>
+      <div>
+        <a class="mixcloud-follow-widget" href="http://www.mixcloud.com/superstar_destroyer/" data-h="160"  data-w="280" data-faces="on">Follow Superstar Destroyer on Mixcloud</a><script type="text/javascript" src="http://widget.mixcloud.com/media/js/follow_embed.js"></script> 
+      </div>
+    </li>
+    <li class="soundcloud"><a href="http://www.soundcloud.com/ssdrecords/" title="Soundcloud"><span class="displace">Soundcloud</span></a>
+      <div style="display:none;">
+        <iframe width="100%" height="450" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F1022161&amp;auto_play=false&amp;show_artwork=true&amp;color=c9140d"></iframe>
+        <a href='http://soundcloud.com/ssdrecords/dropbox' style='display: block; margin: 10px auto; background: transparent url(http://a1.sndcdn.com/images/dropbox_small_white.png?6b62d62) top left no-repeat; color: #888888; font-size: 10px; height: 30px; padding: 26px 60px 0 12px; width: 127px; text-decoration: none; font-family: "Lucida Grande", Helvetica, Arial, sans-serif; line-height: 1.3em' class='soundcloud-dropbox'>Send me your sounds</a>
+      </div>
+    </li>
+    <li class="youtube"><a href="http://www.youtube.com/superstardestroyeruk" title="Youtube Channel"><span class="displace">Youtube</span></a></li>
+    <li class="rss"><a href="https://superstardestroyer.wordpress.com/feed/rss/" title="RSS"><span class="displace">RSS</span></a></li> 
+  </ul>
+</div>
+
+<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
+<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/ssdrecords.json?callback=twitterCallback2&count=3"></script>
